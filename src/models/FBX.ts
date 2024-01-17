97a99a9544cf9a49e153a915d464213ee0ac53f6
Arthur <45236453+ArthurMJ96@users.noreply.github.com>
2024-01-17 15:16:47 +0100

    Support loading local GLTF/FBX model files.

--- src/models/FBX.ts
+++ src/models/FBX.ts
@@ -1,15 +1,39 @@
-import { defineComponent } from 'vue'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
-import Model from './Model'
+import { defineComponent } from "vue";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
+import { knownFolders, path, File } from "@nativescript/core";
+import Model from "./Model";
 
 export default defineComponent({
   extends: Model,
   created() {
-    const loader = new FBXLoader()
-    this.$emit('before-load', loader)
-    loader.load(this.src, (fbx) => {
-      this.onLoad(fbx)
-      this.initObject3D(fbx)
-    }, this.onProgress, this.onError)
+    const loader = new FBXLoader();
+    this.$emit("before-load", loader);
+
+    const normalizedPath: string = path.normalize(
+      knownFolders.currentApp().path + this.src.replace("~", "")
+    );
+    if (File.exists(normalizedPath)) {
+      const file = File.fromPath(normalizedPath);
+      file.read().then((contents) => {
+        const uint8arr: Uint8Array = new Uint8Array(contents);
+        try {
+          const fbx = loader.parse(uint8arr.buffer, normalizedPath);
+          this.onLoad(fbx);
+          this.initObject3D(fbx);
+        } catch (error) {
+          this.onError(error);
+        }
+      });
+    } else {
+      loader.load(
+        this.src,
+        (fbx) => {
+          this.onLoad(fbx);
+          this.initObject3D(fbx);
+        },
+        this.onProgress,
+        this.onError
+      );
+    }
   },
-})
+});
--- src/models/GLTF.ts
+++ src/models/GLTF.ts
@@ -1,7 +1,8 @@
-import { defineComponent } from 'vue'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-import Model from './Model'
+import { defineComponent } from "vue";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { knownFolders, path, File } from "@nativescript/core";
+import Model from "./Model";
 
 export default defineComponent({
   extends: Model,
@@ -9,16 +10,51 @@ export default defineComponent({
     dracoPath: { type: String, required: true },
   },
   created() {
-    const loader = new GLTFLoader()
+    const localPath: string = path.normalize(
+      knownFolders.currentApp().path + this.src.replace("~", "")
+    );
+
+    const localExists = File.exists(localPath);
+
+    const loader = new GLTFLoader();
     if (this.dracoPath) {
-      const dracoLoader = new DRACOLoader()
-      dracoLoader.setDecoderPath(this.dracoPath)
-      loader.setDRACOLoader(dracoLoader)
+      const dracoLoader = new DRACOLoader();
+      dracoLoader.setDecoderPath(
+        localExists
+          ? this.dracoPath
+          : path.normalize(
+              knownFolders.currentApp().path + this.dracoPath.replace("~", "")
+            )
+      );
+      loader.setDRACOLoader(dracoLoader);
     }
-    this.$emit('before-load', loader)
-    loader.load(this.src, (gltf) => {
-      this.onLoad(gltf)
-      this.initObject3D(gltf.scene)
-    }, this.onProgress, this.onError)
+
+    this.$emit("before-load", loader);
+
+    if (localExists) {
+      const file = File.fromPath(localPath);
+      file.read().then((contents) => {
+        const uint8arr: Uint8Array = new Uint8Array(contents);
+        loader.parse(
+          uint8arr.buffer,
+          file.parent.path,
+          (gltf) => {
+            this.onLoad(gltf);
+            this.initObject3D(gltf.scene);
+          },
+          this.onError
+        );
+      });
+    } else {
+      loader.load(
+        this.src,
+        (gltf) => {
+          this.onLoad(gltf);
+          this.initObject3D(gltf.scene);
+        },
+        this.onProgress,
+        this.onError
+      );
+    }
   },
-})
+});
